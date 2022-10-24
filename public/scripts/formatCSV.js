67f45352e2fab799a8e96998e65bc2bcb7504759
Mark Rodriguez <Markrodriguez003@gmail.com>
2022-10-24 16:30:20 -0400

    Added example .csv writer & download link.

--- public/scripts/formatCSV.js
+++ public/scripts/formatCSV.js
@@ -1,81 +1,96 @@
-
 // REFERENCES
 // https://code-maven.com/create-and-download-csv-with-javascript
 // https://www.geeksforgeeks.org/how-to-create-and-download-csv-file-in-javascript/
 
 export default function formatCSV(formData, files) {
+  var formData = {
+    filepath: "/test/hub/folder1",
+    client: "48009",
+    matter: "0009",
+    doctype: "Doc",
+    "document name": "Test.pdf",
+    "document extension": ".pdf",
+    folder: "/netdocs/directoryB",
+  };
 
-    // var testObject = {
-    //     test: true,
-    //     number: 245,
-    //     text: "test"
-    // }
+  var testArry = [
+    "filepath",
+    "client",
+    "matter",
+    "doctype",
+    "document name",
+    "document extension",
+    "folder",
+  ];
 
-  
+  var data = [
+    [
+      "/myfilename/data/mainhub",
+      "49008",
+      "0001",
+      "shapes",
+      ".pdf",
+      "/netdocs/folders/hellow",
+    ],
+    [
+      "/myfilename/data/mainhub",
+      "49008",
+      "0001",
+      "triangles",
+      ".docx",
+      "/netdocs/folders/hellow",
+    ],
+    [
+      "/myfilename/data/mainhub",
+      "49008",
+      "0001",
+      "armitures",
+      ".txt",
+      "/netdocs/folders/hellow",
+    ],
+  ];
 
-    var data = [
-        ["/myfilename/data/mainhub", "49008", "0001", "shapes", ".pdf", "/netdocs/folders/hellow"],
-        ["/myfilename/data/mainhub", "49008", "0001", "triangles", ".docx", "/netdocs/folders/hellow"],
-        ["/myfilename/data/mainhub", "49008", "0001", "armitures", ".txt", "/netdocs/folders/hellow"]
-    ];
-    
-    
-    var csv = 'filepath,client,matter,doctype,document name, document extension, folder,\n';
-  
-   
-    // csv.push(headers.join(','));
-    data.forEach(function(row){
-        csv += row.join(',');
-        csv += "\n";
-
-    });
-
-
- 
- 
-
-    // console.log("Amount of files: " + files.length);
-
-
-    // console.log("Test raw Object: " + testObject);
-    // console.log("Test JSON.Stringified Object: " +JSON.stringify(testObject));
-
-    // console.log("****************************************");
-    // console.log("The single raw files: " + files[1]);
-    // console.log("The single JSON.Stringified file: " + JSON.stringify(files[1]));
-    // console.log("****************************************");
-    // console.log("The Entire raw files: " + files);
-    // console.log("The JSON.Stringified files: " + JSON.stringify(files[1]));
-    // console.log ("whats keys inside: " + Object.keys(files));
-    // console.log ("whats values inside: " + Object.values(files));
-    // console.log ("whats values inside: " + JSON.stringify(Object.values(files)));
-
-
-    // for (var i = 0; i < files.length; i++) {
-    //     formattedData = formattedData +
-    //         "\r" +
-    //         files[i].webkitRelativePath +
-    //         "," + "\r " +
-    //         formData.client +
-    //         "," + "\r" +
-    //         formData.matter +
-    //         "," + "\r" +
-    //         formData.docType +
-    //         "," + "\r" +
-    //         files[i].name +
-    //         "," + "\r" +
-    //         files[i].type +
-    //        "," + "\r" +
-    //         formData.netDocs +
-    //         "," +
-    //         formData.author;
+  var csv = [];
+  console.log("This is what is being pushed into csv --> " + testArry[0])
+  csv.push(testArry[0]);
+  console.log("This is the csv data --> " + csv[0])
 
 
+  // console.log("Amount of files: " + files.length);
 
-    // }
+  // console.log("Test raw Object: " + testObject);
+  // console.log("Test JSON.Stringified Object: " +JSON.stringify(testObject));
 
+  // console.log("****************************************");
+  // console.log("The single raw files: " + files[1]);
+  // console.log("The single JSON.Stringified file: " + JSON.stringify(files[1]));
+  // console.log("****************************************");
+  // console.log("The Entire raw files: " + files);
+  // console.log("The JSON.Stringified files: " + JSON.stringify(files[1]));
+  // console.log ("whats keys inside: " + Object.keys(files));
+  // console.log ("whats values inside: " + Object.values(files));
+  // console.log ("whats values inside: " + JSON.stringify(Object.values(files)));
 
+  // for (var i = 0; i < files.length; i++) {
+  //     formattedData = formattedData +
+  //         "\r" +
+  //         files[i].webkitRelativePath +
+  //         "," + "\r " +
+  //         formData.client +
+  //         "," + "\r" +
+  //         formData.matter +
+  //         "," + "\r" +
+  //         formData.docType +
+  //         "," + "\r" +
+  //         files[i].name +
+  //         "," + "\r" +
+  //         files[i].type +
+  //        "," + "\r" +
+  //         formData.netDocs +
+  //         "," +
+  //         formData.author;
 
+  // }
 
-    return csv ;
+  return csv;
 }
--- public/scripts/writeCSV.js
+++ public/scripts/writeCSV.js
@@ -4,12 +4,13 @@
 // import path from "/node";
 
 export default function writeCSV(data) {
-  console.log("I'm inside writeCSV func!")
 
   const DOWNLOAD_LOADF = document.getElementById("load-f");
 
   console.log("Writing csv load file. . . ");
   var loadFile = null;
+  // var universalBOM = "\uFEFF";
+  // var csvString = 'ı,ü,ü,ğ,ş,#Hashtag,ä,ö';
   var data = new Blob([data], { type: "text/csv" });
 
   if (loadFile !== null) {
@@ -19,6 +20,8 @@ export default function writeCSV(data) {
   loadFile = window.URL.createObjectURL(data);
 
   DOWNLOAD_LOADF.style.display = "none";
+  DOWNLOAD_LOADF.setAttribute('href', 'data:text/csv; charset=utf-8,' + encodeURI(loadFile));
+  // DOWNLOAD_LOADF.setAttribute('download', 'loadfile.csv');
   return loadFile;
 }
 
